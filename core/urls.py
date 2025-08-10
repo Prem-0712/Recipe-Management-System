6627b9da508df1b7ce62d74eb2b4dcf8836256ae
Prem-0712 <premgangurde007@gmail.com>
2025-08-10 08:33:34 +0530

    done

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
 ] 
--- rps/settings.py
+++ rps/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'rps.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "rps",
+        "USER": "Prem",
+        "PASSWORD": "Spidey@07.",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
